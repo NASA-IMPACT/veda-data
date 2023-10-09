9c51d164f61d6e114c5934f5755cb2ae6d3eb0b1
Pete Gadomski <pete.gadomski@gmail.com>
2023-10-09 13:57:14 -0600

    feat: use pytest
    Instead of a custom validate script, just use pytest

---
+++ tests/test_collections.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+import pytest
+from pystac import Collection
+
+ROOT = Path(__file__).parents[1]
+COLLECTIONS_PATH = ROOT / "ingestion-data" / "collections"
+
+
+@pytest.mark.parametrize("path", COLLECTIONS_PATH.rglob("*.json"))
+def test_validate(path: Path) -> None:
+    collection = Collection.from_file(str(path))
+    collection.validate()
