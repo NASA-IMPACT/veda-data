43553b9460633062134ac0c5ce31f5835547a097
Jennifer Tran <12633533+botanical@users.noreply.github.com>
2025-02-13 13:48:20 -0800

    fix: lint

--- scripts/promote_collection.py
+++ scripts/promote_collection.py
@@ -32,21 +32,53 @@ def validate_discovery_item_config(item: Dict[str, Any]) -> Dict[str, Any]:
     return item
 
 
-def promote_collection(stage: str, payload):
-    if stage == "staging":
-        base_api_url = os.getenv("STAGING_SM2A_API_URL")
-        promotion_dag = os.getenv("PROMOTION_DAG_NAME", "veda_promotion_pipeline")
-        username = os.getenv("STAGING_SM2A_ADMIN_USERNAME")
-        password = os.getenv("STAGING_SM2A_ADMIN_PASSWORD")
-    else:
-        base_api_url = os.getenv("SM2A_API_URL")
-        promotion_dag = os.getenv("PROMOTION_DAG_NAME", "veda_promotion_pipeline")
-        username = os.getenv("SM2A_ADMIN_USERNAME")
-        password = os.getenv("SM2A_ADMIN_PASSWORD")
+def publish_to_staging(payload):
+    base_api_url = os.getenv("STAGING_SM2A_API_URL")
+    dataset_pipeline_dag = os.getenv("DATASET_DAG_NAME", "veda_dataset_pipeline")
+    username = os.getenv("STAGING_SM2A_ADMIN_USERNAME")
+    password = os.getenv("STAGING_SM2A_ADMIN_PASSWORD")
 
     api_token = b64encode(f"{username}:{password}".encode()).decode()
-    print(password)
-    print(api_token)
+
+    if not base_api_url or not api_token:
+        raise ValueError(
+            "STAGING_SM2A_API_URL or STAGING_SM2A_ADMIN_USERNAME"
+            + " or STAGING_SM2A_ADMIN_PASSWORD is not"
+            + " set in the environment variables."
+        )
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Basic " + api_token,
+    }
+
+    body = {
+        **payload,
+        "dag_run_id": f"{dataset_pipeline_dag}-{uuid.uuid4()}",
+        "note": "Run from GitHub Actions veda-data",
+    }
+    http_conn = http.client.HTTPSConnection(base_api_url)
+    http_conn.request(
+        "POST",
+        f"/api/v1/dags/{dataset_pipeline_dag}/dagRuns",
+        json.dumps(body),
+        headers,
+    )
+    response = http_conn.getresponse()
+    response_data = response.read()
+    print(f"Response: ${response_data}")
+    http_conn.close()
+
+    return {"statusCode": response.status, "body": response_data.decode()}
+
+
+def promote_to_production(payload):
+    base_api_url = os.getenv("SM2A_API_URL")
+    promotion_dag = os.getenv("PROMOTION_DAG_NAME", "veda_promotion_pipeline")
+    username = os.getenv("SM2A_ADMIN_USERNAME")
+    password = os.getenv("SM2A_ADMIN_PASSWORD")
+
+    api_token = b64encode(f"{username}:{password}".encode()).decode()
 
     if not base_api_url or not api_token:
         raise ValueError(
@@ -59,6 +91,7 @@ def promote_collection(stage: str, payload):
         "Authorization": "Basic " + api_token,
     }
 
+    payload["transfer"] = True
     body = {
         **payload,
         "dag_run_id": f"{promotion_dag}-{uuid.uuid4()}",
@@ -70,7 +103,7 @@ def promote_collection(stage: str, payload):
     )
     response = http_conn.getresponse()
     response_data = response.read()
-    print(f"Response: ${response}")
+    print(f"Response: ${response_data}")
     http_conn.close()
 
     return {"statusCode": response.status, "body": response_data.decode()}
@@ -81,14 +114,15 @@ if __name__ == "__main__":
         with open(sys.argv[1], "r") as file:
             input = json.load(file)
             stage = sys.argv[2]
-            if stage == "production":
-                input["transfer"] = True
             discovery_items = input.get("discovery_items")
             validated_discovery_items = [
                 validate_discovery_item_config(item) for item in discovery_items
             ]
             dag_payload = {"conf": input}
-            promote_collection(stage, dag_payload)
+            if stage == "production":
+                promote_to_production(dag_payload)
+            elif stage == "staging":
+                publish_to_staging(dag_payload)
 
     except IndexError:
         print("Usage: promote_collection.py <file_name> <stage>")
