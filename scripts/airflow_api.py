46f7dba88b453ec179503152e39745f9891a0bf4
Jennifer Tran <12633533+botanical@users.noreply.github.com>
2026-09-17 17:01:03 -0700

    fix: update audience

--- scripts/airflow_api.py
+++ scripts/airflow_api.py
@@ -40,7 +40,7 @@ def _generate_jwt_token(secret: str, expiration_time: int = 3600) -> str:
     payload = {
         "iss": "airflow",
         "sub": "veda-data-client",
-        "aud": "airflow",
+        "aud": "apache-airflow",
         "nbf": int(time.time()),
         "iat": int(time.time()),
         "exp": int(time.time()) + expiration_time,
